0940ad81fc797f2dcaa8b5ba1dc8d9a77870e1bc
hungtv <tranvuhung@outlook.com>
2023-02-24 16:59:13 +0700

    continue fix ui

--- app/src/main/java/com/vuhungtran/happyplaces/MainActivity.kt
+++ app/src/main/java/com/vuhungtran/happyplaces/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.vuhungtran.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vuhungtran.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlace?.setOnClickListener{
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
